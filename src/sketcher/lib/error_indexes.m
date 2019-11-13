0737cb649cebdf85cbffce1665363822c9e0de52
Jeancarlo Hidalgo <jeancahu@gmail.com>
2019-11-13 07:50:34 -0600

    add: semicolons for a quite execution for error_indexes rutine

--- src/sketcher/lib/error_indexes.m
+++ src/sketcher/lib/error_indexes.m
@@ -1,28 +1,36 @@
 TMIN=0; % FIXME
 
-err_min_step = min([X1_t(2) X2_t(2) X3_t(2) X4_t(2)])
-err_index_temp_time = [TMIN:err_min_step:TMAX]
+err_min_step = min([X1_t(2) X2_t(2) X3_t(2) X4_t(2)]);
+err_index_temp_time = [TMIN:err_min_step:TMAX];
 
 %% PI 1.4 Ms
 if PI_Ms_1_4_enable
-    IAE_r = sum(abs(step(Myr_PI_Ms_1_4*k_Ref,err_index_temp_time)))
-    IAE_d = sum(abs(step(Myd_PI_Ms_1_4*k_Per,err_index_temp_time)))
+    IAE_r = sum(abs(step(Myr_PI_Ms_1_4*k_Ref,err_index_temp_time)-1));
+    IAE_d = sum(abs(step(Myd_PI_Ms_1_4*k_Per,err_index_temp_time)));
+
+    PI_Ms_1_4_TOTAL_IAE=IAE_r+IAE_d
 end
 
 %% PI 2.0 Ms
 if PI_Ms_2_0_enable
-    IAE_r = sum(abs(step(Myr_PI_Ms_2_0*k_Ref,err_index_temp_time)))
-    IAE_d = sum(abs(step(Myd_PI_Ms_2_0*k_Per,err_index_temp_time)))
+    IAE_r = sum(abs(step(Myr_PI_Ms_2_0*k_Ref,err_index_temp_time)-1));
+    IAE_d = sum(abs(step(Myd_PI_Ms_2_0*k_Per,err_index_temp_time)));
+
+    PI_Ms_2_0_TOTAL_IAE=IAE_r+IAE_d
 end
 
 %% PID ODoL 1.4 Ms
 if PID_Ms_1_4_enable
-    IAE_r = sum(abs(step(Myr_PID_Ms_1_4*k_Ref,err_index_temp_time)))
-    IAE_d = sum(abs(step(Myd_PID_Ms_1_4*k_Per,err_index_temp_time)))
+    IAE_r = sum(abs(step(Myr_PID_Ms_1_4*k_Ref,err_index_temp_time)-1));
+    IAE_d = sum(abs(step(Myd_PID_Ms_1_4*k_Per,err_index_temp_time)));
+
+    PID_Ms_1_4_TOTAL_IAE=IAE_r+IAE_d
 end
 
 %% PID ODoL 2.0 Ms
 if PID_Ms_2_0_enable
-    IAE_r = sum(abs(step(Myr_PID_Ms_2_0*k_Ref,err_index_temp_time)))
-    IAE_d = sum(abs(step(Myd_PID_Ms_2_0*k_Per,err_index_temp_time)))
+    IAE_r = sum(abs(step(Myr_PID_Ms_2_0*k_Ref,err_index_temp_time)-1));
+    IAE_d = sum(abs(step(Myd_PID_Ms_2_0*k_Per,err_index_temp_time)));
+    
+    PID_Ms_2_0_TOTAL_IAE=IAE_r+IAE_d
 end
