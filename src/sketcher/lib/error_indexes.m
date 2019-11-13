0554e47f873e6700cb120cb6b906541531982c29
Jeancarlo Hidalgo <jeancahu@gmail.com>
2019-11-13 07:45:30 -0600

    fix: error vector range syntax

--- src/sketcher/lib/error_indexes.m
+++ src/sketcher/lib/error_indexes.m
@@ -1,7 +1,7 @@
 TMIN=0; % FIXME
 
 err_min_step = min([X1_t(2) X2_t(2) X3_t(2) X4_t(2)])
-err_index_temp_time = [TMIN;err_min_step;TMAX]
+err_index_temp_time = [TMIN:err_min_step:TMAX]
 
 %% PI 1.4 Ms
 if PI_Ms_1_4_enable
