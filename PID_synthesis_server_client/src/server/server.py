93728a5b2bc51369b826dfc3928dde931bf22670
Jeancahu <jeancahu@gmail.com>
2022-06-03 20:20:30 -0600

    add: update server rutine

--- PID_synthesis_server_client/src/server/server.py
+++ PID_synthesis_server_client/src/server/server.py
@@ -1,49 +1,56 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 from sys import exit
 from sys import argv
+
 from subprocess import Popen as subproc
 from subprocess import PIPE
-from common.client_thread import Client
-from hashlib import sha224 as sha
+
+# from common.client_thread import Client # FIXME
+
 import socket
-import time
 
 ## Init
 if len(argv) < 3:
-  print("Insuficientes argumentos")
+  print("Bad args")
   exit(2)
 
 ## Global Vars:
 server_URL  = str(argv[1])
 server_port = int(argv[2])
 
-def main(): 
-
-  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crear socket server  
+def main():
+  """
+  Create server socket, listen to request one by one and open
+  a thread for every client who connects to the port
+  """
+  server = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM) # Server socker
   try:
     server.bind((server_URL, server_port))
-    print("Se ha iniciado el server con el puerto" , server_port , "como receptor")
+    print("Server is started at port" , server_port)
   except:
-    print("Fue imposible inicializar el server con el puerto", server_port)
+    print("Sever failed when it has been trying to start at port",
+          server_port,
+          "it might have been busy")
     exit(1)
 
-  server.listen(1)
-  clients = [] # Lista de clientes
+  server.listen(1) # Listen to one request at time
+  clients = []     # Clients list
 
-  while True:
+  while True:      # Server keeps waiting for request
     try:
-      socket_cliente, datos_cliente = server.accept()
+      client_socket, client_data = server.accept()
 
     except KeyboardInterrupt:
       for csocket in clients:
-        csocket.join()
-      server.close()
+        csocket.join() # It closes every connection
+      server.close()   # It ends the server rutine
       exit(0)
 
-    client_thread = Client(socket_cliente, datos_cliente)
-    client_thread.start()
-    clients.append(client_thread) # Ingresa el cliente en la lista
+    client_thread = Client(client_socket, client_data)
+    client_thread.start()         # Start client's thread
+    clients.append(client_thread) # It ingress client to the list
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
   main()
