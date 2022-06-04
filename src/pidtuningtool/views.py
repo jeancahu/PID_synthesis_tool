a3a69f6c5e31d562d8f843dcbc4973b87feeead9
Jeancahu <jeancahu@gmail.com>
2022-06-03 23:09:40 -0600

    add: proto view for pid tuning tool app

--- src/pidtuningtool/views.py
+++ src/pidtuningtool/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+# from django.contrib.auth.decorators import login_required # TODO
+from django.views.decorators.http import require_GET, require_POST # TODO
+from .__version__ import __version__ as se_version # TODO
 
-# Create your views here.
+# from django.conf import settings # TODO
+
+@require_GET
+def pidtuningtool(request):
+    context = {
+    }
+    return render(request, 'pidtuningtool/index.html', context)
